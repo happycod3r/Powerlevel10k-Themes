ba03f2a2b468181108b03af598bad7fbfe4faf8b
happycod3r <happy2code0101@gmail.com>
2023-04-14 16:30:26 -0400

    Updated .p10k.zsh file

--- pink_yellow_goo.theme/.p10k.zsh
+++ pink_yellow_goo.theme/.p10k.zsh
@@ -199,7 +199,7 @@
   # Green prompt symbol if the last command succeeded.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=46
   # Red prompt symbol if the last command failed.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=196
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=160 #default =196
   # Default prompt symbol.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION=' ' #default ='❯'
   # Prompt symbol in command vi mode.
